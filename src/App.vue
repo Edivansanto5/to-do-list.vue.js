0f1214e1047acd5e1fcd9a12513561aa30e3c968
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-10 21:31:25 -0300

    finalizando aula

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <toList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import toList from './components/toList.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    toList
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/toList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="conteiner">
+        <h1>teste</h1>
+
+    </div>
+</template>
+<script>
+
+export default {
+    name:'toList',
+   
+}
+</script>
+
+<style scoped>
+    .conteiner{
+        background-color: black;
+    }
+</style>
