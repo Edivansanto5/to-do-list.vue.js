6506a90ee31cf06a3615f0a2ecc8af93e014e783
edivan <edivansantos1478@gmail.com>
2022-10-11 22:14:32 -0300

    finalizando to-do-list

--- src/components/toList.vue
+++ src/components/toList.vue
@@ -2,13 +2,27 @@
   <div class="conteiner">
     <form >
       <h3>Digite um item</h3>
-      <input type="text" v-model="lista">
-        <button type="button" v-on:click="addLista"> Enviar</button>
-        <ul>
-          <li v-for="lista in listas" :key="lista">{{lista}}</li>
-          
-        </ul>
+      <input type="text" v-model.trim="lista.text" placeholder="Digite Qualque coisa">
+        <button type="button" @click="addLista()" > Enviar</button>
+
+        <!-- <ul>
+          <li  v-for="lista in listas" :key="lista">
+            {{ lista.text }}
+            <a href="#" @click="deleta">Excluir</a>
+          </li>
+        </ul> -->
+
+        <div v-for="(lista, index) in listas" :key="index" class="list">
+          <form class="mr-10" >
+            <input :id="index" type="checkbox" v-model="lista.checked" @click="changeChecked(index)" />
+          </form>
+
+          <span :class="listas[index].checked ? 'line-through mr-10' : 'mr-10'" >{{ lista.text }}</span>
+
+          <a class="mr-10" href="#" @click="deleta">Excluir</a>
+        </div>
     </form>
+    <br>
   </div>
     
 </template>
@@ -18,16 +32,31 @@ export default {
   name:'toList', 
   data(){
     return{
-      lista:"",
-      listas:['lista1','lista2','lista3']
+      lista: {
+        checked: false,
+        text: '',
+      },
+
+      listas: [],
     }
   },
   methods:{
     addLista(){
       this.listas.push(this.lista) // add um item na lista
-      this.lista = ""             // remove limpa o canto do imput
+      this.lista = {
+        checked: false,
+        text: '',
+      }           // remove limpa o canto do imput
+    },
+    deleta(){
+      console.log('veio parar aqui');
+    },
 
+    changeChecked (index) {
+      this.listas[index].checked = !this.listas[index].checked
+      console.log(this.listas[index].checked)
     }
+    
     
   }
  
@@ -37,11 +66,11 @@ export default {
 <style>
   body{
     background: #4fc08c;
+    margin: 5px;
   }
   .conteiner{
     background: white;
-    width: 600px;
-    height: 400px;
+    max-height: 500px;
     padding-top: 15px;  
     text-align: center; 
     border-radius: 15px;
@@ -56,4 +85,32 @@ export default {
     margin: 5px;
 
   }
+  ul{
+    
+    text-align: justify;
+
+  }
+  li{
+    padding-top: 10px;
+  }
+  a{
+    color: red;
+  }
+
+  .line-through {
+    text-decoration: line-through;
+    color: rgba(0, 0, 0, 0.3);
+  }
+
+  .list {
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+
+  .mr-10 {
+    margin-right: 10px;
+  }
+  
 </style>
