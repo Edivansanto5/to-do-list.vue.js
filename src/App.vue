1d8718750f4d206cdde2b125e272a2c12bb45a77
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-11 10:50:06 -0300

    importando o novo compomente

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <!-- <toList/> -->
+    <todoList/>
 
     
   </div>
 </template>
 <script>
-
+import todoList from './components/todoList.vue'
 // import toList from './components/toList.vue'
   export default{
     components:{
       // toList,
+      todoList
     }
   }
 </script>
