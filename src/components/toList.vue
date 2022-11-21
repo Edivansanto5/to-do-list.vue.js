7496830036b83c1ce6c28147889e2dff25fd41d6
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-21 19:37:40 -0300

    fazendo lista estaticas

--- src/components/toList.vue
+++ src/components/toList.vue
@@ -2,13 +2,8 @@
   <div class="row">
     <div class="col-3">
       <h3>A fazer</h3>
-      <draggable class="list-group" :list="list1" group="people" @change="log">
-        <input type="text">
-        <div
-          class="list-group-item"
-          v-for="(element, index) in list1"
-          :key="element.name"
-        >
+      <draggable :list="list1" group="people" @change="log">
+        <div class="list-group-item" v-for="(element, index) in list1" :key="element.name">
           {{ element.name }} {{ index }}
         </div>
       </draggable>
@@ -17,28 +12,24 @@
     <div class="col-3">
       <h3>Fazendo</h3>
       <draggable class="list-group" :list="list2" group="people" @change="log">
-        <div
-          class="list-group-item"
-          v-for="(element, index) in list2"
-          :key="element.name"
-        >
+        <div class="list-group-item" v-for="(element, index) in list2" :key="element.name">
           {{ element.name }} {{ index }}
         </div>
       </draggable>
     </div>
 
     <rawDisplayer class="col-3" :value="list1" title="List 1" />
-
     <rawDisplayer class="col-3" :value="list2" title="List 2" />
+
   </div>
 </template>
 <script>
 import draggable from "vuedraggable";
 
 export default {
-  name: "two-lists",
-  display: "Two Lists",
-  order: 1,
+  // name: "two-lists",
+  // display: "Two Lists",
+  // order: 1,
   components: {
     draggable
   },
--- src/components/todoLista.vue
+++ src/components/todoLista.vue
@@ -5,6 +5,11 @@
             <input type="text" placeholder="Digite sua Tarefa aqui">
             <button>Enviar</button>
             <h1>Tarefas a Fazer</h1>
+            <draggable>
+                <div :list="list1">
+
+                </div>
+            </draggable>
         </div>
 
 
@@ -13,12 +18,33 @@
 </template>
 
 <script>
-export default {
-    name:'todoList',
-    data(){
-        return{
+import draggable from 'vuedraggable';
 
-        }
+export default {
+    name: "todoList",
+    data() {
+        return {
+            list1:[
+                {name:'Edivan   Santos', id:0},
+                {name:'Elenildo Santos', id:1},
+                {name:'Elivanda Santos', id:2},
+                {name:'Edilson  Santos', id:3},
+                {name:'Edilane  Santos', id:4}
+            ],
+            list2:[
+                {name:'Maria',id:5},
+                {name:'Antonio Ediglê de Oliveira', id:6},
+                {name:'Luzia Rodrigues dos santos',id:7},
+                {name:'Basta Loneia',id:8},
+                {name:'Querino',id:9},
+                {name:'Antonio',id:10},
+               
+            ]
+
+        };
+    },
+    components: { 
+        draggable 
     }
 }
 </script>
