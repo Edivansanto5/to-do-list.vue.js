38fd6fcfd64cf5cf36a1307ecf07d327557ffcf4
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-23 14:54:26 -0300

    colocando as propriedades nas divs para ficar uma do lado da outra

--- src/components/toList.vue
+++ src/components/toList.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="conteiner">
-    <h1>Todo-List</h1>
+    <div>
+      <h1>Todo-List</h1>
     <input type="text" placeholder="Digite sua Tarefa">
     <button>Enviar</button>
-    <div class="list1">
-   
-      <h3>Lista a Fazer</h3>
-      <draggable :list="list1" group="people" @change="log">
-        <div v-for="(element) in list1" :key="element.name">{{ element.name }}</div> <br>
-      </draggable>
     </div>
    
-
+    <div class="list1">
+      <h3>Lista a Fazer</h3>
+      <draggable :list="list1" group="people" @change="log">
+        <div class="listas" v-for="(element) in list1" :key="element.name">{{ element.name }}</div> 
+      </draggable>
+    </div>
     <div class="list2">
       <h3>Lista em Andamento</h3>
-      <draggable :list="list2" group="people" @change="log">
-        <div v-for="(element) in list2" :key="element.name">{{ element.name }} </div><br>
+      <draggable :list="list2" group="people">
+        <div class="listas" v-for="(element) in list2" :key="element.name">{{ element.name }} </div>
       </draggable>
     </div>
 
     <div class="list3">
       <h3>Lista Concluidas</h3>
       <draggable :list="list3" group="people" @change="log">
-        <div v-for="(element) in list3" :key="element.name">{{ element.name }}</div><br>
+        <div class="listas" v-for="(element) in list3" :key="element.name">{{ element.name }}</div>
       </draggable>
     </div>
   </div>
@@ -32,7 +32,7 @@
 import draggable from "vuedraggable";
 
 export default {
-  // name: "two-lists",
+  // name: "toList",
   // display: "Two Lists",
   // order: 1,
   components: {
@@ -49,12 +49,14 @@ export default {
       list2: [
         { name: "Juan"},
         { name: "Edgard"},
-        { name: "Johnson"}
+        { name: "Johnson"},
+        { name: "Talita"}
       ],
       list3: [
         { name: "Edivan"},
         { name: "Edigar" },
-        { name: "Jumento"}
+        { name: "Jumento"},
+        { name: "Cavalona"}
       ]
     };
   },
@@ -80,14 +82,33 @@ export default {
     body{
       background: #fef6e4;
       font-size: 28px;
+      
     }
-    .list1,.list3,.list2{
+    .list1,.list2,.list3{
       background:#feedc4;
       width: 25%;
       border-radius: 10px;
+      display: inline-block;
+      margin: 10px;
+      
     }
     
     h3{
       text-align: center;
     }
+    .listas{
+      margin: 3px;
+      padding: 2px;
+    }
+    .conteiner{
+      widows: 100%;
+      height: 100%;
+    }
+    .listas{
+      margin: 5px;
+      padding: 5px;
+     
+    }
+    
+  
 </style>
