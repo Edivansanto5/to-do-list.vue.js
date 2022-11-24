6b04d63b6190def8a8918303b6543dc5d307be67
AlessandroVianaSilva <silvaalessandro352@gmail.com>
2022-11-24 13:56:42 -0300

    adding trim e keyup

--- src/components/toList.vue
+++ src/components/toList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="conteiner">
+  <div class="container">
     <div class="box">
       <h1>Todo-List</h1>
-      <input type="text" v-model="lista.text" placeholder="Digite Qualque coisa">
-      <button type="button" @click="addLista()" > Enviar</button>
+      <input type="text" v-model="lista.text" @keyup.enter="addLista(lista.text)" placeholder="Digite Qualquer coisa">
+      <button type="button" @click="addLista(lista.text)" > Enviar</button>
 
       <draggable :lista="listas" group="people" >
         <div class="listas" v-for="(lista, index) in listas" :key="index"> 
@@ -13,21 +13,21 @@
     </div>
 
     <div class="list1">
-      <h3>Lista a Fazer</h3>
+      <h3>Listas a Fazer</h3>
       <draggable :list="list1" group="people" @change="log">
         <div class="listas" v-for="(element) in list1" :key="element.name">{{ element.name }}</div> 
       </draggable>
     </div>
     
     <div class="list2">
-      <h3>Lista em Andamento</h3>
+      <h3>Listas em Andamento</h3>
       <draggable :list="list2" group="people">
        
       </draggable>
     </div>
 
     <div class="list3">
-      <h3>Lista Concluidas</h3>
+      <h3>Listas Concluidas</h3>
       <draggable :list="list3" group="people" @change="log">
        
       </draggable>
@@ -54,14 +54,24 @@ export default {
   components: {
     draggable
   },
-  methods:{
-    addLista(){
-      this.listas.push(this.lista) // add um item na lista
-      this.lista = {
-        text: '',
-      }     
+  methods:{   
+      addLista(lista){
+
+        if(lista.trim().length) {
+
+          this.listas.push(this.lista) // add um item na lista
+          this.lista = {
+            text: '',
+          }
+          }  else {
+          alert('Por favor, entre com caracteres validos')
+        }   
+      },
     },
-  },
+    // removeLista(lista) {
+    //   if(lista) {
+    //   }
+    // }
 
 };
 </script>
@@ -84,7 +94,7 @@ export default {
     margin: 3px;
     padding: 2px;
   }
-  .conteiner{
+  .container{
     widows: 100%;
     height: 100%;
   }
---
+++ src/components/todoList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="conteiner">
+        <div class="box">
+            <h1>{{title}}</h1>
+        </div> <br> 
+        <form class="tarefas">
+            <h1>{{nome}}</h1>
+            <button class="btn" type="###">Status</button>
+            <input type="text" placeholder="Digite aqui uma Tarefa"> 
+            <button class="btn" @click="addLista()">Adcionar</button>
+           
+            
+            <br><br>
+            <!-- <img src="../assets/excluir.png" @click="apertou()"> -->
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'todoList',
+    data(){
+        return{
+
+            title:'TO-DO-LIST',
+            nome:'Bem-vindo Ao Gerênciamento de Tarefas',
+            status:['A Fazer','Fazendo','Feito'],
+           
+        }
+    },
+   
+}
+</script>
+
+<style>
+    .conteiner {   
+        background: white;
+        width: 600px;
+        height: 80px;
+        padding-top: 15px;  
+        text-align: center; 
+        border-radius: 15px;
+   
+    }
+    .tarefas{
+        background: white;
+        width: 600px;
+        padding-top: 15px;  
+        text-align: center; 
+        border-radius: 15px;
+    }
+    body{
+        background: #4fc08c;
+     
+    }
+    .select,.btn{
+        padding-left: 10px;
+        margin: 12px;
+    }
+    img{
+        width: 40px;
+        
+    }
+
+    
+
+    
+</style>
